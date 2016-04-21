a524c251a2f7b571c7936fc83e243a830a1e4244
SuDaming <1436267481@qq.com>
2016-04-21 22:28:19 +0800

    加入显示天气信息的功能，以及新增切换城市和手动更新天气的功能。

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -27,6 +30,10 @@ import com.coolweather.app.util.Utility;
 
 public class ChooseAreaActivity extends Activity
 {
+	/**
+	 * 是否从WeatherActivity中跳转过来。
+	 */
+	private boolean isFromWeatherActivity;
 	public static final int LEVEL_PROVINCE = 0;
 	public static final int LEVEL_CITY = 1;
 	public static final int LEVEL_COUNTY = 2;
@@ -65,6 +72,19 @@ public class ChooseAreaActivity extends Activity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		isFromWeatherActivity = getIntent().getBooleanExtra(
+				"from_weather_activity", false);
+
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		// 已经选择了城市且不是从WeatherActivity跳转过来，才会直接跳转到 WeatherActivity
+		if (prefs.getBoolean("city_selected", false) && !isFromWeatherActivity)
+		{
+			Intent intent = new Intent(this, WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView = (ListView) findViewById(R.id.list_view);
@@ -89,9 +109,19 @@ public class ChooseAreaActivity extends Activity
 					selectedCity = cityList.get(index);
 					queryCounties();
 				}
+				else if (currentLevel == LEVEL_COUNTY)
+				{
+					String countyCode = countyList.get(index).getCountyCode();
+					Intent intent = new Intent(ChooseAreaActivity.this,
+							WeatherActivity.class);
+					intent.putExtra("county_code", countyCode);
+					startActivity(intent);
+					finish();
+				}
 			}
 		});
 		queryProvinces(); // 加载省级数据
+
 	}
 
 	/**
@@ -288,6 +318,11 @@ public class ChooseAreaActivity extends Activity
 		}
 		else
 		{
+			if (isFromWeatherActivity)
+			{
+				Intent intent = new Intent(this, WeatherActivity.class);
+				startActivity(intent);
+			}
 			finish();
 		}
 	}
